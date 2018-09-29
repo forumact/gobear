5dc1455b547a008e3c6eaddc7001409ee7e9ac71
arulraj <arulraj@1046944.no-reply.drupal.org>
2018-09-29 17:10:05 +0530

    assessment-arulraj

--- web/modules/custom/gobear_jobs/js/gobear_jobs.js
+++ web/modules/custom/gobear_jobs/js/gobear_jobs.js
@@ -5,9 +5,9 @@
 
 (function ($, window, Drupal) {
 
-  'use strict';
+    'use strict';
 
-  /**
+    /**
    * Provide the basic function on job listing.
    *
    * @type {Drupal~behavior}
@@ -15,20 +15,24 @@
    * @prop {Drupal~behaviorAttach} attach
    *   Attaches the behavior for the jobs listing.
    */
-  Drupal.behaviors.Gobeaer = {
-    attach: function () {
-	  $('body').once('Gobeaer').each(function() {	
-		  $('.toggle-desc').click(function(e) {              
-			 if($(this).find('.clicktext').text() == 'More Info'){
-			   $(this).find('.clicktext').text('Less Info');
-			 }else{
-			   $(this).find('.clicktext').text('More Info');	 
-			 }			 
-			 $(this).parents('.card-body').find('.long-desc').toggleClass('hide');	
-             $(this).parents('.card-body').find('.short-desc').toggleClass('hide');	          		     
-		  });
-	  });
-    }
-  };
+    Drupal.behaviors.Gobeaer = {
+        attach: function () {
+            $('body').once('Gobeaer').each(
+                function() {    
+                    $('.toggle-desc').click(
+                        function(e) {              
+                            if($(this).find('.clicktext').text() == 'More Info') {
+                                 $(this).find('.clicktext').text('Less Info');
+                            }else{
+                                $(this).find('.clicktext').text('More Info');     
+                            }             
+                            $(this).parents('.card-body').find('.long-desc').toggleClass('hide');    
+                            $(this).parents('.card-body').find('.short-desc').toggleClass('hide');                           
+                        }
+                    );
+                }
+            );
+        }
+    };
 
 })(jQuery, window, Drupal);
--- web/modules/custom/gobear_jobs/src/Controller/GobearjobsController.php
+++ web/modules/custom/gobear_jobs/src/Controller/GobearjobsController.php
@@ -5,69 +5,70 @@ namespace Drupal\gobear_jobs\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\Client;
-
+ 
 /**
  * Class GobearjobsController.
  */
-class GobearjobsController extends ControllerBase {
+class GobearjobsController extends ControllerBase
+{
 
-  /**
+    /**
    * GuzzleHttp\Client definition.
    *
    * @var \GuzzleHttp\Client
    */
-  protected $httpClient;
+    protected $httpClient;
 
-  /**
+    /**
    * Constructs a new GobearjobsController object.
    */
-  public function __construct(Client $http_client) {
-    $this->httpClient = $http_client;
-  }
+    public function __construct(Client $http_client) 
+    {
+        $this->httpClient = $http_client;
+    }
 
-  /**
+    /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('http_client')
-    );
-  }
+    public static function create(ContainerInterface $container) 
+    {
+        return new static(
+        $container->get('http_client')
+        );
+    }
 
-  /**
+    /**
    * Joblisting.
    *
    * @return string
    *   Return Hello string.
    */
-  public function jobListing() {
-    // $request = $this->httpClient->createRequest('GET', 'https://jobs.github.com/positions.json?location=new+york');
-    // $request->addHeader('If-Modified-Since', gmdate(DATE_RFC1123, $last_fetched));.
-    $data = NULL;
-    try {
-      $response = $this->httpClient->get('https://jobs.github.com/positions.json?location=new+york', [
-        'headers' => [
-          'Accept' => 'application/json',
-        ],
-      ]);
-      // Expected result.
-      // getBody() returns an instance of Psr\Http\Message\StreamInterface.
-      // @see http://docs.guzzlephp.org/en/latest/psr7.html#body
-      $data = $response->getBody()->getContents();
+    public function jobListing() 
+    {
+        $data = null;
+        try {
+            $response = $this->httpClient->get(
+                'https://jobs.github.com/positions.json?location=new+york', [
+                'headers' => [
+                'Accept' => 'application/json',
+                ],
+                ]
+            );
+            // Expected result.
+            // getBody() returns an instance of Psr\Http\Message\StreamInterface.
+            // @see http://docs.guzzlephp.org/en/latest/psr7.html#body
+            $data = $response->getBody()->getContents();
       
-	  
-      $decoded = (isset($data)) ? json_decode($data) : NULL;
-	  Echo '<pre>';
-	  print_r($decoded);
-	  Echo '</pre>';
-      return [
-        '#theme' => 'gobear_jobs',
-        '#data' => $decoded,		
-      ];
+      
+            $decoded = (isset($data)) ? json_decode($data) : null;
+            return [
+            '#theme' => 'gobear_jobs',
+            '#data' => $decoded,        
+            ];
+        }
+        catch (RequestException $e) {
+            watchdog_exception('Gobearjobs', $e);
+        }
     }
-    catch (RequestException $e) {
-      watchdog_exception('Gobearjobs', $e);
-    }
-  }
 
 }
